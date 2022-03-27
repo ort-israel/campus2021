5246f1e4d434c22c18bbf3e9d2c204f619ab3efe
ORT Developer <intertech@ort.org.il>
2022-03-27 09:14:22 +0300

    add theme bediukcity

---
+++ theme/bediukcity/lib/filesettings_lib.php
@@ -0,0 +1,74 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * FileSettings Lib file.
+ *
+ * @package     theme_bediukcity
+ * @copyright  2016 Chris Kenniburg
+ * @credits    theme_boost - MoodleHQ
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+/**
+ * Serves any files associated with the theme settings.
+ *
+ * @param stdClass $course
+ * @param stdClass $cm
+ * @param context $context
+ * @param string $filearea
+ * @param array $args
+ * @param bool $forcedownload
+ * @param array $options
+ * @return bool
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * This function creates the dynamic HTML needed for some
+ * settings and then passes it back in an object so it can
+ * be echo'd to the page.
+ *
+ * This keeps the logic out of the layout files.
+ *
+ * @param string $setting bring the required setting into the function
+ * @param bool $format
+ * @param string $setting
+ * @param array $theme
+ * @param stdclass $CFG
+ * @return string
+ */
+function theme_bediukcity_get_setting($setting, $format = false) {
+    global $CFG;
+    require_once($CFG->dirroot . '/lib/weblib.php');
+    static $theme;
+    if (empty($theme)) {
+        $theme = theme_config::load('bediukcity');
+    }
+    if (empty($theme->settings->$setting)) {
+        return false;
+    } else if (!$format) {
+        return $theme->settings->$setting;
+    } else if ($format === 'format_text') {
+        return format_text($theme->settings->$setting, FORMAT_PLAIN);
+    } else if ($format === 'format_html') {
+        return format_text($theme->settings->$setting, FORMAT_HTML, array('trusted' => true, 'noclean' => true));
+    } else {
+        return format_string($theme->settings->$setting);
+    }
+}
